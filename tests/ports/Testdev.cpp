90546790bd3d906575ecdd67288ceaf8b225051a
Gabriel Schreiber <akira.schreiber@gmail.com>
2015-09-15 09:55:28 +0200

    Failing unit tests for state class. state.set does not propagate to copied

--- tests/ports/Testdev.cpp
+++ tests/ports/Testdev.cpp
@@ -12,14 +12,32 @@ BOOST_AUTO_TEST_CASE( fetcher_ )
 	BOOST_CHECK(sink.get() == 1);
 }
 
-BOOST_AUTO_TEST_CASE( state_ )
+BOOST_AUTO_TEST_CASE( state_fetcher_direct_connection )
+{
+	state<int> source {1};
+	fetcher<int> sink;
+	source >> sink;
+	BOOST_CHECK(sink.get() == 1);
+	source.set(2);
+	BOOST_CHECK(sink.get() == 2);
+}
+
+BOOST_AUTO_TEST_CASE( state_multiple_fetchers_and_assignment )
 {
 	auto increment = [](int i) -> int {return i+1;};
 	state<int> source {1};
-	fetcher<int> sink;
-	source >> increment >> sink;
-	BOOST_CHECK(sink.get() == 2);
+	fetcher<int> sink1;
+	fetcher<int> sink2;
+	source >> increment >> increment >> sink1;
+	source >> increment >> increment >> sink2;
+	BOOST_CHECK(sink1.get() == 3);
+	BOOST_CHECK(sink2.get() == 3);
+	source.set(5);
+	BOOST_CHECK(sink1.get() == 7);
+	BOOST_CHECK(sink2.get() == 7);
 }
+
+
 
 
 
