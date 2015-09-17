172425520d402bd7a9ec047215fd7489f24a2bb9
Caspar Kielwein <caspar@kielwein.de>
2015-09-17 16:09:32 +0200

    added test for split and merge of events

--- tests/ports/TestEvents.cpp
+++ tests/ports/TestEvents.cpp
@@ -55,4 +55,58 @@ BOOST_AUTO_TEST_CASE(test_event_connections)
 	BOOST_CHECK_EQUAL(*(test_handler.storage), 4);
 }
 
+template<class T>
+struct event_vector_sink
+{
+	void operator()(T in)
+	{
+		storage->push_back(in);
+	}
+	std::shared_ptr<std::vector<T>> storage =
+			std::shared_ptr<std::vector<T>>(new std::vector<T>);
+};
+
+namespace fc{
+
+template<class T>
+struct is_event_sink<event_vector_sink<T>> : public std::true_type
+{
+};
+}
+
+BOOST_AUTO_TEST_CASE(merge_events)
+{
+	event_out_port<int> test_event;
+	event_out_port<int> test_event_2;
+	event_vector_sink<int> test_handler;
+
+	test_event >> test_handler;
+	test_event_2 >> test_handler;
+
+	test_event.fire(0);
+	BOOST_CHECK_EQUAL(test_handler.storage->size(), 1);
+	BOOST_CHECK_EQUAL(test_handler.storage->back(), 0);
+
+	test_event_2.fire(1);
+
+	BOOST_CHECK_EQUAL(test_handler.storage->size(), 2);
+	BOOST_CHECK_EQUAL(test_handler.storage->front(), 0);
+	BOOST_CHECK_EQUAL(test_handler.storage->back(), 1);
+
+}
+
+BOOST_AUTO_TEST_CASE(split_events)
+{
+	event_out_port<int> test_event;
+	event_sink<int> test_handler_1;
+	event_sink<int> test_handler_2;
+
+	test_event >> test_handler_1;
+	test_event >> test_handler_2;
+
+	test_event.fire(2);
+	BOOST_CHECK_EQUAL(*(test_handler_1.storage), 2);
+	BOOST_CHECK_EQUAL(*(test_handler_2.storage), 2);
+}
+
 #endif /* TESTS_CORE_TESTEVENTS_CPP_ */
