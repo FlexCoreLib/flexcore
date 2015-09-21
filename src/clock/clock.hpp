9f38c03246c537d5b565c89e987e2e89ddaeebe4
Caspar Kielwein <caspar@kielwein.de>
2015-09-21 14:34:10 +0200

    added base files for development of clock.

---
+++ src/clock/clock.hpp
@@ -0,0 +1,15 @@
+/*
+ * clock.hpp
+ *
+ *  Created on: Sep 21, 2015
+ *      Author: ckielwein
+ */
+
+#ifndef SRC_CLOCK_CLOCK_HPP_
+#define SRC_CLOCK_CLOCK_HPP_
+
+
+
+
+
+#endif /* SRC_CLOCK_CLOCK_HPP_ */
---
+++ tests/clock/TestClock.cpp
@@ -0,0 +1,15 @@
+/*
+ * Unit Tests for our virtual clock.
+ */
+
+// boost
+#include <boost/test/unit_test.hpp>
+
+#include <clock/clock.hpp>
+
+using namespace fc;
+
+BOOST_AUTO_TEST_CASE(test_example_uses)
+{
+
+}
