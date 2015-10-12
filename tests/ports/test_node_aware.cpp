934987e1596384bba6602ba2c867a82b9ee31cc6
Holger Berg <hberg2010@web.de>
2015-10-12 17:43:53 +0200

    use sink buffer to test sequential generation output in test_node_aware.

--- tests/ports/test_node_aware.cpp
+++ tests/ports/test_node_aware.cpp
@@ -17,7 +17,7 @@ BOOST_AUTO_TEST_CASE(test_region_aware_node)
 	test_in_port test_in(region, write_param);
 
 	BOOST_CHECK_EQUAL(test_value, 0);
-	test_in.operator ()(1);
+	test_in(1);
 	BOOST_CHECK_EQUAL(test_value, 1);
 }
 
@@ -28,8 +28,8 @@ BOOST_AUTO_TEST_CASE(test_same_region)
 	typedef region_aware<event_out_port<int>> test_out_port;
 	auto region = std::make_shared<parallel_region>();
 
-	int test_value = 0;
-	auto write_param = [&](int i) {test_value = i;};
+	std::vector<int> test_sink;
+	auto write_param = [&](int i) {test_sink.push_back(i);};
 	test_in_port test_in(region, write_param);
 	test_out_port test_out(region);
 
@@ -39,15 +39,16 @@ BOOST_AUTO_TEST_CASE(test_same_region)
 
 	test_out >> test_in;
 
-	BOOST_CHECK_EQUAL(test_value, 0);
+	BOOST_CHECK_EQUAL(test_sink.size(), 0);
 	test_out.fire(1);
-	BOOST_CHECK_EQUAL(test_value, 1);
+	BOOST_CHECK_EQUAL(test_sink.at(0), 1);
 
 	auto tmp = test_out >> [](int i ){ return ++i;};
 	tmp >> test_in;
 
 	test_out.fire(1);
-	BOOST_CHECK_EQUAL(test_value, 2);
+	BOOST_CHECK_EQUAL(test_sink.at(1), 1);
+	BOOST_CHECK_EQUAL(test_sink.at(2), 2);
 }
 
 BOOST_AUTO_TEST_CASE(test_different_region)
@@ -105,7 +106,7 @@ BOOST_AUTO_TEST_CASE(test_connectable_in_between)
 	test_in_port test_in_2(region_3, write_param);
 	test_out >>
 			[](int i){ return i+1;} >>
-			[](int i){ return i+1;} >>
+			[](int i){ return i*2;} >>
 			test_in_2;
 
 	test_out.fire(1);
@@ -115,7 +116,7 @@ BOOST_AUTO_TEST_CASE(test_connectable_in_between)
 	BOOST_CHECK_EQUAL(test_value, 2);
 
 	region_3->ticks.in_work()();
-	BOOST_CHECK_EQUAL(test_value, 3);
+	BOOST_CHECK_EQUAL(test_value, 4);
 }
 
 BOOST_AUTO_TEST_CASE(test_state_transition)
