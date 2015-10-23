49a69f03092bfcfdc2282a8d3b415d5927748390
Caspar Kielwein <caspar@kielwein.de>
2015-10-23 17:28:39 +0200

    class documentation of active_connection_proxy

--- src/core/detail/active_connection_proxy.hpp
+++ src/core/detail/active_connection_proxy.hpp
@@ -58,14 +58,28 @@ struct active_source_first
 };
 
 /**
- * \brief connection of an active_connectable && a connectable
- * fulfills active_connectable
- */
+ * \brief contains connections of active_connectables during creation.
+ *
+ * active_connection_proxy is used to store the intermediate objects created,
+ * when a connection is build up from an active connectable
+ * Until the active connectable is connected to a proper passive,
+ * the connection is not complete and no value can be pulled or send through.
+ * Nonetheless, the connection needs to be stored to allow further connections.
+ * the active_connection_proxy stores these temporary objects.
+ *
+ * \tparam active_ is the active connection,
+ * \tparam passive_t the passive side of the connection durint buildup
+ * \tparam
+ * {
+ * connect_policy a policy class which makes sure parameters to connect
+ * inside the proxy are in the correct order.
+ * }
+ * */
 template<class active_t, class passive_t, class connect_policy>
 struct active_connection_proxy
 {
 	static_assert(is_active<active_t>::value,
-			"active_t needs to be active connectable");
+			"active_t in proxy needs to be active connectable");
 
 	typedef typename
 			connect_policy::template first<active_t, passive_t>::result_t payload_t;
@@ -77,21 +91,43 @@ struct active_connection_proxy
 			stored_passive(passive)
 	{}
 
+	/**
+	 * \brief connects a passive connectable to the active_connection_proxy.
+	 *
+	 * connects the new_source to the current stored source
+	 * then connects this connection to the sink which completes the connection
+	 *
+	 * \pre new_passive_t needs to be passive connectable.
+	 * \post new_passive is now connected to active via the connection stored in the proxy.
+	 * \returns port connection tag object.
+	 */
 	template<class new_passive_t, class enable = void>
 	auto connect(new_passive_t new_passive,
 			typename std::enable_if< is_passive<new_passive_t>::value>::type* = 0)
 	{
+		static_assert(is_passive<new_passive_t>::value,
+				"new_passive_t in proxy needs to be passive connectable");
+
 		auto tmp = connect_policy()(stored_passive, new_passive); //todo order of parameters
 
 		return active.connect(tmp);
 	}
 
-	template<	class new_passive_t,
-				class = typename std::enable_if< not is_passive<new_passive_t>::value >::type
+	/**
+	 * \brief connects a connectable, which is not a passive connectable to the active_connection_proxy.
+	 *
+	 * connects the new_connectable to the current stored source
+	 *
+	 * \pre new_connectable_t needs to be connectable
+	 * \post new_connectable_t is connected to the old passive.
+	 * \returns a active_connection_proxy, which contains the new_connectable in its connection.
+	 */
+	template<	class new_connectable_t,
+				class = typename std::enable_if< not is_passive<new_connectable_t>::value >::type
 			>
-	auto connect(new_passive_t new_passive)
+	auto connect(new_connectable_t new_connectable)
 	{
-		auto connection = connect_policy()(stored_passive, new_passive); //todo order of parameters
+		auto connection = connect_policy()(stored_passive, new_connectable); //todo order of parameters
 		return active_connection_proxy<
 				active_t,
 				decltype(connection),
@@ -125,8 +161,8 @@ struct active_passive_connect_impl
 };
 
 /**
- * Specialization of active_passive_connect_impl for the case of connecting a st&&ard connectable
- * which is not a active_port to a passive_port.
+ * Specialization of active_passive_connect_impl for the case of connecting a standard connectable
+ * which is not a is_passive_sink to a is_passive_source.
  * \return a stream_proxy which contains the active && the passive.
  */
 template<class active_t, class passive_t, class argument_order>
@@ -153,10 +189,10 @@ struct active_passive_connect_impl
 
 /**
  * Specialization for the case of connecting a active_port to a passive_port.
- * \pre active_t needs to be a stream_active.
- * \pre passive_t needs to be a stream_passive.
+ * \pre active_t needs to be a active_connectable.
+ * \pre passive_t needs to be a passive_connectable.
  * \post active is now connected to passive
- * \return nothing, the connection is complete
+ * \return port_connection tag object with the type information of the connection.
  */
 template<class active_t, class passive_t, class argument_order>
 struct active_passive_connect_impl
