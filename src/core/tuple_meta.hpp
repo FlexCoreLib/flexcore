7a69c58945bb18dfba018072f8622f47c70764e4
Caspar Kielwein <caspar@kielwein.de>
2015-11-04 18:01:24 +0100

    new small functions to operate on std tuple

---
+++ src/core/tuple_meta.hpp
@@ -0,0 +1,78 @@
+/*
+ * tuple_meta.hpp
+ *
+ *  Created on: Nov 4, 2015
+ *      Author: ckielwein
+ */
+
+#ifndef SRC_CORE_TUPLE_META_HPP_
+#define SRC_CORE_TUPLE_META_HPP_
+
+namespace fc
+{
+namespace tuple
+{
+
+namespace detail
+{
+
+template<class lhs_tuple, class rhs_tuple, std::size_t ... index,
+class operation>
+decltype(auto) binary_invoke_helper(lhs_tuple&& lsh,
+		rhs_tuple&& rhs,
+		std::index_sequence<index...>,
+		operation&& op)
+{
+	return std::make_tuple(op(
+					std::get<index>(std::forward<lhs_tuple>(lsh)),
+					std::get<index>(std::forward<rhs_tuple>(rhs)))...);
+}
+
+template<class lhs_tuple, std::size_t ... index,
+class operation>
+
+decltype(auto) unary_invoke_helper(lhs_tuple&& lsh,
+		std::index_sequence<index...>,
+		operation&& op)
+{
+	std::make_tuple(op(
+					std::get<index>(std::forward<lhs_tuple>(lsh)))...);
+}
+} //namespace detail
+
+/**
+ * Selection of metafunctions using std::tuple.
+ * Basically a very minimal subset of boost::fusion
+ */
+
+///applies function to every element in tuple
+template<class tuple, class operation>
+void for_each(tuple&& tup, operation&& op)
+{
+unary_invoke_helper(tup,
+        std::make_index_sequence<std::tuple_size<tuple>::value> { }, op);
+}
+
+///transform, returns tuple of transformed elements
+template<class tuple, class operation>
+decltype(auto) transform(const tuple& tup, const operation& op)
+{
+	return detail::unary_invoke_helper(tup,
+			std::make_index_sequence<std::tuple_size<tuple>::value> { }, op);
+}
+///binary_transform, returns tuple of results of bin_op on elements of first and second tuple
+template<class first_tuple, class second_tuple, class operation>
+decltype(auto) transform(const first_tuple& first, const second_tuple& second, const operation& op)
+{
+	static_assert(std::tuple_size<first_tuple>::value ==
+			std::tuple_size<second_tuple>::value ,
+			"Binary tuple transform needs tuples to have same nr of elements.");
+
+	return detail::binary_invoke_helper(first, second,
+			std::make_index_sequence<std::tuple_size<first_tuple>::value> { }, op);
+}
+
+}  // namespace tuple
+}  // namespace fc
+
+#endif /* SRC_CORE_TUPLE_META_HPP_ */
