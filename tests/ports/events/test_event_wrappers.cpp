79089b15b1da9c066389b7cfdf3c362dae8db65b
Justus Schwan <justus.schwan@gmail.com>
2015-11-24 14:43:14 +0100

    Cleaned up test cases

--- tests/ports/events/test_event_wrappers.cpp
+++ tests/ports/events/test_event_wrappers.cpp
@@ -20,18 +20,11 @@ using namespace fc;
 template<class T>
 struct wrapper_event_sink
 {
-	wrapper_event_sink(std::string name):
-	mName(name)
-	{
-	}
-
 	void operator()(T in)
 	{
-//		*storage = in;
-		std::cout<<"Sink "<<mName<<" says "<<in<<"\n";
+		*storage = in;
 	}
-//	std::shared_ptr<T> storage = std::make_shared<T>();
-	std::string mName;
+	std::shared_ptr<T> storage = std::make_shared<T>();
 };
 
 
@@ -40,14 +33,14 @@ BOOST_AUTO_TEST_SUITE(test_event_wrappers)
 BOOST_AUTO_TEST_CASE(test_wrapper_callback)
 {
 	event_source_wrapper<event_out_port<int>> test_source;
-	event_sink_wrapper<wrapper_event_sink<int>> test_sink1("Sink 1");
-	event_sink_wrapper<wrapper_event_sink<int>> test_sink4("Sink 4");
+	event_sink_wrapper<wrapper_event_sink<int>> test_sink1;
+	event_sink_wrapper<wrapper_event_sink<int>> test_sink4;
 	test_source >> test_sink1;
 	test_source.fire(5);
 
 	{
-		event_sink_wrapper<wrapper_event_sink<int>> test_sink2("Sink 2");
-		event_sink_wrapper<wrapper_event_sink<int>> test_sink3("Sink 3");
+		event_sink_wrapper<wrapper_event_sink<int>> test_sink2;
+		event_sink_wrapper<wrapper_event_sink<int>> test_sink3;
 		test_source >> test_sink2;
 		test_source >> test_sink3;
 		test_source.fire(6);
