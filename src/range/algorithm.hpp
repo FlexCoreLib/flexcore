0c9c96194a2382e5cf8a2b81fbf73af8e6d682bf
Caspar Kielwein <caspar@kielwein.de>
2015-11-25 09:26:18 +0100

    added connectables for map, filter and reduce on ranges

--- src/range/algorithm.hpp
+++ src/range/algorithm.hpp
@@ -1,17 +1,75 @@
 #ifndef SRC_RANGE_ALGORITHM_HPP_
 #define SRC_RANGE_ALGORITHM_HPP_
 
+#include <boost/range.hpp>
+#include <boost/range/adaptors.hpp>
+
+#include <functional>
+
 namespace fc {
 
-template<class range, class algorithm>
-struct range_view
+template<class predicate>
+struct filter_view
 {
-	template<class param, class result>
-	decltype(auto) operator()(const param&& p)
+	template<class in_range>
+	auto operator()(const in_range& input)
 	{
-		return algorithm(std::forward(p));
+		return boost::adaptors::filter(input, pred);
 	}
+	predicate pred;
 };
+
+template<class predicate>
+auto filter(predicate pred)
+{
+	return filter_view<predicate>{pred};
+}
+
+template<class operation>
+struct map_view
+{
+	template<class in_range>
+	auto operator()(const in_range& input)
+	{
+		return boost::adaptors::transform(input, op);
+	}
+	operation op;
+};
+
+template<class operation>
+auto map(operation op)
+{
+	return map_view<operation>{op};
+}
+
+template<class binop, class T>
+struct reduce_view
+{
+	template<class in_range>
+	auto operator()(const in_range& input)
+	{
+		return std::accumulate(begin(input), end(input), init_value, op);
+	}
+	binop op;
+	T init_value;
+};
+
+template<class binop, class T>
+auto reduce(binop op, T initial_value)
+{
+	return reduce_view<binop, T>{op, initial_value};
+}
+
+template<class T>
+auto sum(T initial_value = T())
+{
+	return reduce(std::plus<>(), initial_value);
+}
+
+
+
+
+
 
 }  // namespace fc
 
--- tests/range/test_range.cpp
+++ tests/range/test_range.cpp
@@ -1,45 +1,33 @@
 #include <boost/test/unit_test.hpp>
 
-#include <3rdparty/range-v3/include/range/v3/all.hpp>
+//#include <3rdparty/range-v3/include/range/v3/all.hpp>
 
 #include <core/connection.hpp>
+#include <range/algorithm.hpp>
 
 using namespace fc;
-using namespace ranges;
 
 BOOST_AUTO_TEST_SUITE(test_rage)
 
 BOOST_AUTO_TEST_CASE(test_algorithm)
 {
-    auto tmp = view::ints(1)
-                       | view::transform([](int i){return i*i;})
-    				   | view::transform([](int i){return i*2;});
+	std::vector<int> vec = {-4, -3, -2, -1, 0, 1, 2, 3, 4};
 
-    auto result = tmp | view::take(10);
+	auto source = [&vec](){ return boost::make_iterator_range(std::begin(vec), std::end(vec)); };
+	auto sink = [](auto in){ for(int i : in) { std::cout << i << ", ";}};
 
-    std::cout << result << "\n";
+	auto connection = source
+				>> filter([](int i){ return i < 0;})
+				>> map([](int i) { return i*2;})
+				>> sink;
+	connection();
 
-    std::vector<int> bla = {1, 2, 3, 4, 5} ;
-
-   std::vector<int> temp2 = std::move(bla) | action::transform([](int a){return a+1;});
-
-    for (auto a : bla)
-    	std::cout << "bla: " << a << "\n";
-
-    for (auto a : temp2)
-        	std::cout << "temp2: " << a << "\n";
-
-
-
-    //auto connection = [](){ return view::ints(1); }
-    //		>> [](auto&& in ){ return view::transform([](int i){return i*i;})(in);};
-//    		>> [](auto&& in ){ return view::take(10)(in); }
-//    		>> [](auto&& in ){ std::cout << "range connection:\n" << in << "\n"; };
-
-    //auto blaasd = connect([](){ return view::ints(1); },  [](auto&& in ){ return view::transform([](int i){return i*i;})(in);} );
-
-    //connection();
-  //  blaasd();
+	auto connection_2 = source
+			>> filter([](int i){ return i < 0;})
+			>> map([](int i){ return i*2;})
+			>> sum(0)
+			>> [](int i){std::cout << "sum: " << i << "\n"; };
+	connection_2();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
