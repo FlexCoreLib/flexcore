7e27d46e158a9fdb275cb203f5eaed8ea3d72b6d
Caspar Kielwein <caspar@kielwein.de>
2015-11-25 11:31:14 +0100

    removed range library for now, will be included again later

--- tests/range/test_range.cpp
+++ tests/range/test_range.cpp
@@ -1,33 +1,33 @@
 #include <boost/test/unit_test.hpp>
 
-#include <3rdparty/range-v3/include/range/v3/all.hpp>
+//#include <3rdparty/range-v3/include/range/v3/all.hpp>
 
 #include <core/connection.hpp>
 
 using namespace fc;
-using namespace ranges;
+//using namespace ranges;
 
 BOOST_AUTO_TEST_SUITE(test_rage)
 
 BOOST_AUTO_TEST_CASE(test_algorithm)
 {
-    auto tmp = view::ints(1)
-                       | view::transform([](int i){return i*i;})
-    				   | view::transform([](int i){return i*2;});
-
-    auto result = tmp | view::take(10);
-
-    std::cout << result << "\n";
-
-    std::vector<int> bla = {1, 2, 3, 4, 5} ;
-
-   std::vector<int> temp2 = std::move(bla) | action::transform([](int a){return a+1;});
-
-    for (auto a : bla)
-    	std::cout << "bla: " << a << "\n";
-
-    for (auto a : temp2)
-        	std::cout << "temp2: " << a << "\n";
+//    auto tmp = view::ints(1)
+//                       | view::transform([](int i){return i*i;})
+//    				   | view::transform([](int i){return i*2;});
+//
+//    auto result = tmp | view::take(10);
+//
+//    std::cout << result << "\n";
+//
+//    std::vector<int> bla = {1, 2, 3, 4, 5} ;
+//
+//   std::vector<int> temp2 = std::move(bla) | action::transform([](int a){return a+1;});
+//
+//    for (auto a : bla)
+//    	std::cout << "bla: " << a << "\n";
+//
+//    for (auto a : temp2)
+//        	std::cout << "temp2: " << a << "\n";
 
 
 
