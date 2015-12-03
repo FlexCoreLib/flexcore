73238fdb06c92d02827e6fec4e2cba230e9cce0c
Caspar Kielwein <caspar@kielwein.de>
2015-12-03 12:53:16 +0100

    added second port to unit test of config_file

--- tests/serialisation/test_configfile.cpp
+++ tests/serialisation/test_configfile.cpp
@@ -11,15 +11,22 @@ BOOST_AUTO_TEST_SUITE(test_serialisation)
 BOOST_AUTO_TEST_CASE(test_configfile)
 {
 	std::stringstream ss;
-	ss << "{ \"test_int\": 1 }";
+	ss << "{ "
+			"\"test_int\": 1,"
+			"\"test_float\": 0.5 "
+			"}";
 
 	config_file test_config{ss};
 
 
 	auto out_port = test_config.out_value<int>("test_int");
+	auto out_float = test_config.out_value<double>("test_float");
 	test_config.load();
 
 	BOOST_CHECK_EQUAL(out_port(), 1);
+	BOOST_CHECK_EQUAL(out_float(), 0.5);
+
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
