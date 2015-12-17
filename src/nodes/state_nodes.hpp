5f22a55120fbb4bd562a385759f400aacace78aa
Caspar Kielwein <caspar@kielwein.de>
2015-12-17 10:31:58 +0100

    added current_state and state_cache nodes for generic caching of states

--- src/nodes/state_nodes.hpp
+++ src/nodes/state_nodes.hpp
@@ -3,6 +3,9 @@
 
 #include <core/traits.hpp>
 #include <ports/states/state_sink.hpp>
+#include <ports/states/state_sources.hpp>
+
+#include <ports/events/event_sinks.hpp>
 
 #include <utility>
 
@@ -76,6 +79,82 @@ auto merge(operation op)
 			>(op);
 }
 
+
+/*****************************************************************************/
+/*                                   Caches                                  */
+/*****************************************************************************/
+
+/// pulls inputs on incoming pull tick and makes it available to state otuput out().
+template<class data_t>
+class current_state
+{
+public:
+	explicit current_state(const data_t& initial_value = data_t()) :
+			pull_tick([this]()
+			{
+				out_port.set(in_port.get());
+			}),
+			in_port(),
+			out_port(initial_value)
+	{
+	}
+
+	///events to this port trigger the node to pull its inputs. expects void
+	auto pull() noexcept { return pull_tick; }
+	///State Input Port of type data_t
+	auto in() noexcept { return in_port; }
+	///State Output Port of type data_t
+	auto out() noexcept { return out_port; }
+
+private:
+	event_in_port<void> pull_tick;
+	state_sink<data_t> in_port;
+	state_source_with_setter<data_t> out_port;
+};
+
+/**
+ * \brief Caches state and only pulls new state when cache is marked as dirty.
+ *
+ * switch_tick port needs to be connected,
+ * as events to this port mark the cache as dirty.
+ */
+template<class data_t>
+class state_cache
+{
+public:
+	state_cache() :
+		cache(std::make_shared<data_t>()),
+		load_new(true)
+	{
+	}
+
+	///State Output Port of type data_t
+	auto out() noexcept
+	{
+		return [this]()
+		{
+			if (load_new)
+				refresh_cache();
+			return *cache;
+		};
+	}
+
+	///State Input Port of type data_t
+	auto in() noexcept { return in_port; }
+
+	///events to this port mark the cache as dirty. Expects events of type void.
+	auto switch_tick() noexcept { return [this](){ load_new = true; }; }
+
+private:
+	void refresh_cache()
+	{
+		*cache = in_port.get();
+		load_new = false;
+	}
+	std::shared_ptr<data_t> cache;
+	bool load_new;
+	state_sink<data_t> in_port;
+};
 
 }  // namespace fc
 
--- tests/nodes/test_state_nodes.cpp
+++ tests/nodes/test_state_nodes.cpp
@@ -15,4 +15,49 @@ BOOST_AUTO_TEST_CASE(test_merge)
 	BOOST_CHECK_EQUAL(multiply(), 6);
 }
 
+BOOST_AUTO_TEST_CASE(test_state_cache)
+{
+	state_cache<int> cache;
+
+	int test_val = 1;
+
+	[&test_val](){ return test_val;} >> cache.in();
+	BOOST_CHECK_EQUAL(cache.out()(), 1);
+
+	test_val = 0;
+	BOOST_CHECK_EQUAL(cache.out()(), 1); //cache has not been reset
+
+	cache.switch_tick()(); //reset cache
+	BOOST_CHECK_EQUAL(cache.out()(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_current_state)
+{
+	{ //check constructor
+	current_state<int> test_node_1{1};
+	BOOST_CHECK_EQUAL(test_node_1.out()(), 1);
+
+	current_state<int> test_node_2;
+	BOOST_CHECK_EQUAL(test_node_2.out()(), 0); //default is int() == 0
+	}
+
+	current_state<int> test_node;
+	BOOST_CHECK_EQUAL(test_node.out()(), 0);
+
+	int test_val = 1;
+	auto source = [&test_val](){ return test_val;};
+	source >> test_node.in();
+
+	// we haven't pulled yet
+	BOOST_CHECK_EQUAL(test_node.out()(), 0);
+
+	test_node.pull()();
+	BOOST_CHECK_EQUAL(test_node.out()(), 1);
+
+	test_val = 2;
+	BOOST_CHECK_EQUAL(test_node.out()(), 1);
+	test_node.pull()();
+	BOOST_CHECK_EQUAL(test_node.out()(), 2);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
