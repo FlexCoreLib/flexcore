f8b33ba918456e3a175d908be4f488af649a7952
Caspar Kielwein <caspar@kielwein.de>
2016-01-27 11:41:24 +0100

    First running draft of graph nodes for generic connectables.

--- src/graph/graph.hpp
+++ src/graph/graph.hpp
@@ -70,7 +70,28 @@ public:
 				dataflow_graph);
 	}
 
-	auto& get_boost_graph() { return dataflow_graph; }
+	void add_connection(const graph_node_properties& source_node,
+			const graph_node_properties& sink_node)
+	{
+		//ToDo guard with mutex
+
+		//check if vertex is already included, as add_vertex would add it again.
+		if (vertex_map.find(source_node.get_id()) == vertex_map.end())
+			vertex_map.emplace(
+					source_node.get_id(),
+					boost::add_vertex(vertex{source_node.name()}, dataflow_graph));
+		if (vertex_map.find(sink_node.get_id()) == vertex_map.end())
+			vertex_map.emplace(
+					sink_node.get_id(),
+					boost::add_vertex(vertex{sink_node.name()}, dataflow_graph));
+		boost::add_edge(
+				vertex_map[source_node.get_id()],
+				vertex_map[sink_node.get_id()],
+				edge{" to: "},
+				dataflow_graph);
+	}
+
+	dataflow_graph_t& get_boost_graph() { return dataflow_graph; }
 
 private:
 	connection_graph()
@@ -97,6 +118,19 @@ inline void ad_to_graph(const graph_node_properties& source_node,
 			source_port,
 			sink_node,
 			sink_port);
+}
+
+/**
+ * \brief Adds a new edge to the graph representation.
+ *
+ * The edge is leading from source to sink without ports.
+ */
+inline void ad_to_graph(const graph_node_properties& source_node,
+		const graph_node_properties& sink_node)
+{
+	connection_graph::access().add_connection(
+			source_node,
+			sink_node);
 }
 
 inline void print()
--- src/graph/graph_connectable.hpp
+++ src/graph/graph_connectable.hpp
@@ -1,17 +1,71 @@
 #ifndef SRC_GRAPH_GRAPH_CONNECTABLE_HPP_
 #define SRC_GRAPH_GRAPH_CONNECTABLE_HPP_
 
+#include <graph/graph.hpp>
+#include <core/connection.hpp>
+
 namespace fc
 {
 namespace graph
 {
 
+template<class base>
+struct graph_connectable : public base
+{
+	explicit graph_connectable(const base& args)
+		: base(args)
+		, graph_info("Todo")
+	{
+	}
 
+	graph_connectable(const base& args, const graph_node_properties& graph_info)
+		: base(args)
+		, graph_info(graph_info)
+	{
+	}
+
+	graph_node_properties graph_info;
+};
+
+template<class base_t>
+auto make_graph_connectable(const base_t& base,
+		const graph_node_properties& graph_info)
+{
+	return graph_connectable<base_t>{base, graph_info};
+}
+
+template<class connectable>
+auto named(const connectable& con, const std::string& name)
+{
+	return graph_connectable<connectable>{con, graph_node_properties{name}};
+}
+
+template<class source_base, class sink_t>
+auto connect(graph_connectable<source_base> source, sink_t sink)
+{
+	return make_graph_connectable(
+			::fc::connect<source_base, sink_t>(source, sink),
+			 source.graph_info);
+}
+
+template<class source_t, class sink_base>
+auto connect(source_t source, graph_connectable<sink_base> sink)
+{
+	return make_graph_connectable(
+			::fc::connect<source_t, sink_base>(source, sink),
+			 sink.graph_info);
+}
+
+template<class source_base, class sink_base>
+auto connect(graph_connectable<source_base> source,
+		graph_connectable<sink_base> sink)
+{
+	//add edge to graph with node info of source and sink
+	ad_to_graph(source.graph_info, sink.graph_info);
+	return ::fc::connect<source_base, sink_base>(source, sink);
+}
 
 }  // namespace graph
-
 }  // namespace fc
-
-
 
 #endif /* SRC_GRAPH_GRAPH_CONNECTABLE_HPP_ */
