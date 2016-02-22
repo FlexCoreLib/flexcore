af340d9564f605eb3775d961a7cd99c8c36d816f
Jeremi Piotrowski <jeremi.piotrowski@gmail.com>
2016-02-22 15:29:29 +0100

    tests: use log_client in stream_logging test.

--- tests/logging/test_logging.cpp
+++ tests/logging/test_logging.cpp
@@ -1,19 +1,16 @@
 #define BOOST_ALL_DYN_LINK
 #include <boost/test/unit_test.hpp>
 #include <logging/logger.hpp>
-#include <boost/log/sources/logger.hpp>
-#include <boost/log/common.hpp>
 #include <sstream>
 
 BOOST_AUTO_TEST_CASE( stream_logging )
 {
 	std::ostringstream stream;
-	auto* logger = fc::logger::get();
-	auto handle =
-	    logger->add_stream_log(stream, fc::logger::flush::true_, fc::logger::cleanup::true_);
-	boost::log::sources::logger lg;
+	auto handle = fc::logger::get()->add_stream_log(stream, fc::logger::flush::true_,
+	                                                fc::logger::cleanup::true_);
+	fc::log_client client;
 	auto msg = "test log entry.";
-	BOOST_LOG(lg) << msg;
+	client.write(msg);
 	auto str = stream.str();
 	BOOST_CHECK_NE(str.find(msg), std::string::npos);
 }
