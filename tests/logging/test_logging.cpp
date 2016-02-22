4c89422708661ec5f44b7dfd508eadb972e4af97
Jeremi Piotrowski <jeremi.piotrowski@gmail.com>
2016-02-22 18:09:12 +0100

    tests: convert logging test to fixture test.

--- tests/logging/test_logging.cpp
+++ tests/logging/test_logging.cpp
@@ -3,14 +3,30 @@
 #include <logging/logger.hpp>
 #include <sstream>
 
-BOOST_AUTO_TEST_CASE( stream_logging )
+using fc::logger;
+
+struct log_test
 {
+	log_test()
+	    : handle(
+	          logger::get()->add_stream_log(stream, logger::flush::true_, logger::cleanup::true_))
+	{
+	}
+	~log_test()
+	{
+		auto str = stream.str();
+		BOOST_CHECK_NE(str.find(expected_in_output), std::string::npos);
+		BOOST_TEST_MESSAGE(str);
+	}
 	std::ostringstream stream;
-	auto handle = fc::logger::get()->add_stream_log(stream, fc::logger::flush::true_,
-	                                                fc::logger::cleanup::true_);
+	std::string expected_in_output;
+	fc::stream_handle handle;
+};
+
+BOOST_FIXTURE_TEST_CASE( stream_logging, log_test )
+{
 	fc::log_client client;
-	auto msg = "test log entry.";
-	client.write(msg);
-	auto str = stream.str();
-	BOOST_CHECK_NE(str.find(msg), std::string::npos);
+	expected_in_output = "test log entry.";
+	client.write(expected_in_output);
 }
+
