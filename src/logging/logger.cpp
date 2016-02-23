fb5dfad95511f93b49576db397be234567b25eb2
Jeremi Piotrowski <jeremi.piotrowski@gmail.com>
2016-02-23 16:52:50 +0100

    logging: add documentation.
    Includes API docs, slight implementation documentation and a brief
    introduction into boost.log.

--- src/logging/logger.cpp
+++ src/logging/logger.cpp
@@ -15,6 +15,29 @@
 #include <ios>
 #include <utility>
 
+/**
+ * Boost log has the following design:
+ *
+ * sink_backend - sink_frontend --\          /-- logging source
+ *                                |-- core --|
+ * sink_backend - sink_frontend --/          \-- logging source
+ *
+ * The logging source creates a *record* which contains *attributes* (lineID, Timestamp, Channel,
+ * Severity, ThreadID, Message, etc.). This logging record is passed to the core which passes it on
+ * to the sink_frontends.
+ *
+ * The sink frontends can *filter* the records and select the ones which get passed on to the
+ * backends for outputting (global filtering is also possible in the core). The sink frontends also
+ * do the *formatting* of the record.
+ *
+ * The logging sources are responsible for filling the records and their attributes. Global
+ * attributes (filled automatically) can be registered in the core.
+ *
+ * Sinks need to be registered in the core, sources can be directly created.
+ *
+ * With this information anyone working on this file should know where to look to change the
+ * flexcore logger's behaviour.
+ */
 namespace fc
 {
 
@@ -33,6 +56,8 @@ logger* logger::get()
 	return &instance;
 }
 
+/// Print the severity level to the ostream, required for boost.log. For now this just casts the
+/// value to an integer value, but it could also be converted to some string representation.
 std::ostream& operator<<(std::ostream& out, fc::level severity)
 {
 	out << static_cast<int>(severity);
@@ -46,9 +71,11 @@ using sync_sink = sinks::synchronous_sink<backend>;
 
 namespace
 {
+// keywords for accessing the specified attributes of the log record.
 BOOST_LOG_ATTRIBUTE_KEYWORD(region, "Channel", std::string);
 BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", level);
 
+/// format the log record with severity level, region information and a message.
 auto get_format()
 {
 	namespace expr = boost::log::expressions;
@@ -56,6 +83,8 @@ auto get_format()
 	                    << "[" << region << "] " << expr::smessage;
 }
 
+/// format the log record the with a timestamp and the same information as get_format().
+//TODO: find out how to reuse the output of get_format().
 auto get_format_with_timestamp()
 {
 	namespace expr = boost::log::expressions;
@@ -68,6 +97,7 @@ auto get_format_with_timestamp()
 
 void logger::add_file_log(std::string filename)
 {
+	// append to file, never truncate.
 	auto file_sink = boost::make_shared<sinks::text_file_backend>(
 	    keywords::file_name = filename,
 	    keywords::open_mode = std::ios_base::app | std::ios_base::out);
@@ -78,6 +108,7 @@ void logger::add_file_log(std::string filename)
 
 void logger::add_syslog_log(std::string progname)
 {
+	// use the native api implementation does not use a local network connection.
 	auto syslog_sink = boost::make_shared<sinks::syslog_backend>(
 	    keywords::use_impl = sinks::syslog::impl_types::native,
 	    keywords::ident = progname);
@@ -97,7 +128,7 @@ stream_handle logger::add_stream_log(std::ostream& stream, logger::flush flush,
 	sink_front->set_formatter(get_format_with_timestamp());
 	core::get()->add_sink(sink_front);
 
-	// Prepare a function that will remove the stream from the sink
+	// Prepare a function that will remove the stream from the sink if cleanup is needed.
 	std::function<void()> cleanup_fun;
 	if (static_cast<bool>(cleanup))
 		cleanup_fun = [=]() { stream_sink->remove_stream(shared_stream); };
@@ -109,6 +140,7 @@ stream_handle logger::add_stream_log(std::ostream& stream, logger::flush flush,
 
 logger::logger()
 {
+	// add the time stamp to every record (this does not mean that it gets output automatically).
 	core::get()->add_global_attribute("TimeStamp", attributes::utc_clock{});
 }
 
@@ -146,6 +178,8 @@ log_client::log_client(const log_client& other)
 {
 }
 
+// copy/move and swap idiom. For rvalues this results in two moves instead of one but unique_ptr
+// moves are cheap.
 log_client& log_client::operator=(log_client other)
 {
 	std::swap(log_client_pimpl, other.log_client_pimpl);
--- src/logging/logger.hpp
+++ src/logging/logger.hpp
@@ -11,6 +11,10 @@
 namespace fc
 {
 
+/**
+ * \brief Enumeration of severity levels corresponding to the posix syslog api.
+ * See man 3 syslog.
+ */
 enum class level : char
 {
 	emergency = LOG_EMERG,
@@ -23,6 +27,11 @@ enum class level : char
 	debug = LOG_DEBUG
 };
 
+/**
+ * \brief A handle that will run the deleter function on destruction.
+ * This is used to make sure that a stream that is added to the logger using add_stream_log will be
+ * deregistered before it goes out of scope.
+ */
 class stream_handle
 {
 public:
@@ -40,7 +49,7 @@ private:
 };
 
 /**
- * \brief A class for writing messages to multiple logs.
+ * \brief A class for writing messages to multiple logs backends.
  */
 class logger
 {
@@ -48,12 +57,24 @@ public:
 	/// get the singleton instance of logger.
 	static logger* get();
 
-	/// add log files of the specified type
+	/// add log backends of the specified types
 	void add_file_log(std::string filename);
 	void add_syslog_log(std::string progname);
 
 	enum class flush { true_ = true, false_ = false };
 	enum class cleanup { true_ = true, false_ = false };
+	/**
+	 * \brief add an ostream backend to the logger.
+	 * The reference to the stream will be stored, so make sure that the stream is (either one):
+	 *     a) a global.
+	 *     b) will be removed from the logger before the stream is destroyed.
+	 *
+	 * \param stream The stream to which to write.
+	 * \param flush Flush to the stream on every log write.
+	 * \param cleanup Remove the stream from the logger when the returned stream_handle is destroyed.
+	 *
+	 * \returns A stream_handle that does (or does not) perform cleanup on destruction.
+	 */
 	stream_handle add_stream_log(std::ostream& stream, logger::flush flush,
 	                             logger::cleanup cleanup);
 
@@ -61,11 +82,20 @@ private:
 	logger();
 };
 
+/**
+ * \brief A client of the logger.
+ * This class allows to write log messages to the registered logger backends.  The client is not
+ * MT-safe but models a value type, so a copy of a client can safely be used in another thread.
+ */
+
 class log_client
 {
 public:
+	/// Write msg to the log with the specified severity level.
 	void write(const std::string& msg, level = level::info);
+	/// Construct a log_client with the region name "null"
 	log_client();
+	/// Construct a log_client which logs from the passed region.
 	log_client(const region_info* region);
 
 	log_client(const log_client&);
