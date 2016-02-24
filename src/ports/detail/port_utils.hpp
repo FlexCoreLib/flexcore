873b3db6c2a5f9448d13e908d78db1baa34983fe
Jeremi Piotrowski <jeremi.piotrowski@gmail.com>
2016-02-24 13:58:58 +0100

    ports: handler_wrapper: use std::ref instead of lambda and add comment.
    A std::ref object is as good a forwarding lambda. The context of why the
    handler_wrapper was written is explained in a comment block before the
    functions. tldr: lvalues may be noncopyable, need to be stored in
    something copyable to work with std::function.

--- src/ports/detail/port_utils.hpp
+++ src/ports/detail/port_utils.hpp
@@ -7,18 +7,21 @@
 #ifndef SRC_PORTS_PORT_UTILS_HPP_
 #define SRC_PORTS_PORT_UTILS_HPP_
 
+#include <functional>
+
 namespace fc
 {
 namespace detail
 {
 
+// std::function requires that the stored object be copyable. Once we arrive at the active port's
+// connect member function, the argument is either an rvalue copyable object or an lvalue
+// potentially move only type. So for lvalues it is necessary to construct a copyable wrapper
+// (forwarding lambda or std::ref will do).
 template <class conn_t>
 auto handler_wrapper(conn_t& c)
 {
-	return [&c](auto&&... args)
-	{
-		return c(std::forward<decltype(args)>(args)...);
-	};
+	return std::ref(c);
 }
 template <class conn_t, class enable = std::enable_if_t<std::is_rvalue_reference<conn_t&&>{}>>
 decltype(auto) handler_wrapper(conn_t&& c)
