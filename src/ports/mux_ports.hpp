e67fac4d3a3230303b86bf9f244b709f5a6a182e
Jeremi Piotrowski <jeremi.piotrowski@gmail.com>
2016-03-22 13:10:14 +0100

    ports: mux: add API documentation.

--- src/ports/mux_ports.hpp
+++ src/ports/mux_ports.hpp
@@ -19,6 +19,8 @@ struct default_tag {};
 struct merge_tag {};
 struct mux_tag {};
 
+/// Traits used to determine whether a muxed port is connecting to a muxed
+/// port, a merge port or any other connectable object.
 template <typename T>
 struct port_traits
 {
@@ -39,6 +41,7 @@ struct port_traits<unloaded_merge_port<op>>
 
 namespace detail
 {
+/// Helper: check if any of the passed bools are true.
 template <bool... vals>
 constexpr bool any()
 {
@@ -48,12 +51,19 @@ constexpr bool any()
 			return true;
 	return false;
 }
+
+/** \brief Helper: return false regardless of what the parameters are.
+ *
+ * Useful for static_assert conditions in templates that are always invalid -
+ * prevents them from firing before the template is instantiated.
+ */
 template <typename... T>
 constexpr bool always_false_fun(T...)
 {
 	return false;
 }
 
+/// Helper: check if all of the passed bools are true.
 template <bool... vals>
 constexpr bool all()
 {
@@ -72,6 +82,7 @@ struct many_to_many_tag {};
 struct one_to_many_tag {};
 struct many_to_one_tag {};
 
+/// Trait used to determine how many muxed ports are being connected on each side.
 template <size_t left_ports, size_t right_ports>
 struct mux_traits
 {
@@ -95,12 +106,36 @@ struct mux_traits<1, ports>
 	using connection_category = one_to_many_tag;
 };
 
+/** \brief A wrapper around multiple ports that simplifies establishing
+ *         *identical* connections.
+ *
+ * Basically will lead to the following transformation:
+ *
+ *     mux(a,b,c) >> d       ===>      a >> d
+ *                                     b >> d
+ *                                     c >> d
+ *
+ * with special cases for mux(...) >> mux(...), and mux(...) >> merge_port.
+ *
+ * Mux ports can be used on both sides of a connection but need to be at the
+ * ends of a connection chain. Mux ports don't fulfill the concept of
+ * connectable (or active connectable for that matter) but they do have special
+ * overloads of operator>> which allow them to participate in connections.
+ */
 template <class... port_ts>
 struct mux_port
 {
 	std::tuple<port_ts...> ports;
 
 private:
+	/** \brief Overload for connecting to an unloaded_merge_port
+	 *
+	 * T is expected to be an instantiation of unloaded_merge_port.
+	 * T and T.merge are assumed to be copyable.
+	 *
+	 * \returns loaded_merge_port with the supplied merge operation and the
+	 *          ports previously held by *this.
+	 */
 	template <class T>
 	auto connect(T t, merge_tag)
 	{
@@ -112,6 +147,18 @@ private:
 		return loaded_merge_port<decltype(t.merge), port_ts...>{t.merge, std::move(ports)};
 	}
 
+	/** \brief Overload for connecting to another muxed port.
+	 *
+	 * other_mux is expected to be an instantiation of mux_port, the ports
+	 * in *this and in other should have compatible types.
+	 * The amount of ports on both sides also needs to be compatible:
+	 *
+	 *   * N - N
+	 *   * 1 - N
+	 *   * N - 1
+	 *
+	 * for N > 1. The appropriate overload of connect_mux is selected using mux_traits.
+	 */
 	template <class other_mux>
 	auto connect(other_mux&& other, mux_tag)
 	{
@@ -121,6 +168,11 @@ private:
 		return connect_mux(std::forward<other_mux>(other), connection_tag{});
 	}
 
+	/** \brief Connect each port in *this with each port in other.
+	 *
+	 * mux-mux connections should be the end of a connection chain.
+	 * \returns (if used correctly) std::tuple<port_connections...>
+	 */
 	template <class other_mux_port>
 	auto connect_mux(other_mux_port&& other, many_to_many_tag)
 	{
@@ -132,6 +184,13 @@ private:
 		                            pairwise_connect);
 	}
 
+	/** \brief Connect each port in *this with the one (sink) port in other.
+	 *
+	 * The sink port is copied if it was held by value inside other, or else it
+	 * will be passed to the connection as an lvalue.
+	 *
+	 * \returns std::tuple<port_connections...>
+	 */
 	template <class sink_t>
 	auto connect_mux(mux_port<sink_t>&& other, many_to_one_tag)
 	{
@@ -143,6 +202,13 @@ private:
 		return fc::tuple::transform(std::move(ports), all_to_sink);
 	}
 
+	/** \brief Connect each port in other with the (source) port in *this.
+	 *
+	 * The source port will be copied, for each connection, if it was held by
+	 * value inside *this, otherwise it will be connected as an lvalue.
+	 *
+	 * \returns std::tuple<port_connections...>
+	 */
 	template <class other_mux_port>
 	auto connect_mux(other_mux_port&& other, one_to_many_tag)
 	{
@@ -158,6 +224,11 @@ private:
 		return fc::tuple::transform(std::forward<other_mux_port>(other).ports, src_to_all);
 	}
 
+	/** \brief Overload for connecting all ports in *this with a connectable.
+	 *
+	 * \param T the connectable - will be copied unless it is an lvalue.
+	 * \returns mux_port<connection...> (or mux_port<port_connection...>)
+	 */
 	template <class T>
 	auto connect(T&& t, default_tag)
 	{
@@ -171,6 +242,7 @@ private:
 	}
 
 public:
+	/// Function to forward to the correct connect overload based on the type of T.
 	template <class T>
 	auto operator>>(T&& t)
 	{
@@ -180,6 +252,7 @@ public:
 	}
 };
 
+/// Connect src with all ports in mux. Useful only with event_sources.
 template <class T, class... ports,
           class = std::enable_if_t<is_active_source<std::remove_reference_t<T>>{} &&
                                    detail::all<is_connectable<ports>{}...>()>>
@@ -188,6 +261,10 @@ auto operator>>(T&& src, mux_port<ports...> mux)
 	return mux_port<T>{std::forward_as_tuple(std::forward<T>(src))} >> std::move(mux);
 }
 
+/** \brief Create a mux port from lvalue references to the supplied ports.
+ *
+ * \params ports should be non-const lvalue references to ports.
+ */
 template <class... port_ts>
 auto mux(port_ts&... ports)
 {
@@ -200,12 +277,18 @@ mux_port<conn_ts...> mux_from_tuple(std::tuple<conn_ts...> tuple_)
 	return mux_port<conn_ts...>{std::move(tuple_)};
 }
 
+/// A merge port that is not connected to a mux_port yet. Think of this as a proxy.
 template <class merge_op>
 struct unloaded_merge_port
 {
 	merge_op merge;
 };
 
+/** \brief A merge_port with a merge operation and a set of ports (connectables).
+ *
+ * If a connectable is held by value then loaded_merge_port is the owner of
+ * that connectable.
+ */
 template <class merge_op, class... port_ts>
 struct loaded_merge_port
 {
@@ -224,6 +307,7 @@ struct loaded_merge_port
 	}
 };
 
+/// Create a merge_port proxy that only holds the merge operation.
 template <class merge_op>
 auto merge(merge_op op)
 {
