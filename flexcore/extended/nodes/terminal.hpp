8ce4abc44966ca03f3d2861b5c1297fb9d862ba0
Caspar Kielwein <Caspar@Kielwein.de>
2016-04-07 10:03:38 +0200

    Added terminal nodes
    Added two new classes, terminal state and event node.
    These are used to seperate internal and external connections of a nested node.

---
+++ flexcore/extended/nodes/terminal.hpp
@@ -0,0 +1,92 @@
+/*
+ * terminal.hpp
+ *
+ *  Created on: Apr 7, 2016
+ *      Author: ckielwein
+ */
+
+#ifndef FLEXCORE_EXTENDED_NODES_TERMINAL_HPP_
+#define FLEXCORE_EXTENDED_NODES_TERMINAL_HPP_
+
+#include <flexcore/extended/base_node.hpp>
+
+namespace fc
+{
+
+/**
+ * \brief Node that relays state from input port to output port.
+ *
+ * A state_terminal is useful to form the border of external connections
+ * and internal connections in a compound node which nests other nodes.
+ *
+ * \tparam T type of data transmitted through terminal node.
+ * \tparam base_node base_node to either include or exclude this node from forest.
+ * Instantiate terminal with either pure_node or tree_base_node-
+ */
+template <class T, class base_node = tree_base_node>
+class state_terminal : public base_node
+{
+public:
+	/**
+	 * \brief Constructor which forwards arguments to base_node
+	 * \param args Arguments for Base node. In the default case this is:
+	 * \code {std::shared_ptr<fc::parallel_region> r, const std::string& name}
+	 */
+	template<class... base_args>
+	explicit state_terminal(base_args&&... args)
+		: base_node(std::forward<base_args>(args)...)
+		, in_state(this)
+		, out_state(this, [this](){ return in_state.get(); })
+	{
+	}
+
+	/// State sink of type T
+	auto& in() { return in_state; }
+	/// State source of type T
+	auto& out() { return out_state; }
+
+private:
+	typename base_node::template state_sink<T> in_state;
+	typename base_node::template state_source<T> out_state;
+};
+
+/**
+ * \brief Node that relays events from input port to output port.
+ *
+ * An event_terminal is useful to form the border of external connections
+ * and internal connections in a compound node which nests other nodes.
+ *
+ * \tparam T type of data transmitted through terminal node.
+ * \tparam base_node base_node to either include or exclude this node from forest.
+ * Instantiate terminal with either pure_node or tree_base_node-
+ */
+template <class T, class base_node = tree_base_node>
+class event_terminal : public base_node
+{
+public:
+	/**
+	 * \brief Constructor which forwards arguments to base_node
+	 * \param args Arguments for Base node. In the default case this is:
+	 * \code {std::shared_ptr<fc::parallel_region> r, const std::string& name}
+	 */
+	template<class... base_args>
+	explicit event_terminal(base_args&&... args)
+		: base_node(std::forward<base_args>(args)...)
+		, in_event(this, [this](T in){ out_event.fire(std::move(in));})
+		, out_event(this)
+	{
+	}
+
+	/// Event sink of type T
+	auto& in() { return in_event; }
+	/// Event source of type T
+	auto& out() { return out_event; }
+
+private:
+	typename base_node::template event_sink<T> in_event;
+	typename base_node::template event_source<T> out_event;
+};
+
+}  // namespace fc
+
+#endif /* FLEXCORE_EXTENDED_NODES_TERMINAL_HPP_ */
---
+++ tests/extended/nodes/test_terminal_node.cpp
@@ -0,0 +1,77 @@
+#include <boost/test/unit_test.hpp>
+#include <flexcore/extended/nodes/terminal.hpp>
+#include <flexcore/ports.hpp>
+#include <flexcore/pure/pure_node.hpp>
+
+
+#include <nodes/owning_node.hpp>
+
+using namespace fc;
+
+BOOST_AUTO_TEST_SUITE(test_terminal_node)
+
+BOOST_AUTO_TEST_CASE(test_event_terminal)
+{
+	const float test_val = 1.234;
+
+	{
+	float test_out = 0.0;
+
+	event_terminal<float, pure::pure_node> pure_terminal;
+
+	pure::event_source<float> pure_source;
+	pure::event_sink<float> pure_sink{[&test_out](float in)
+			{test_out = in;}};
+
+	pure_source >> pure_terminal.in();
+	pure_terminal.out() >> pure_sink;
+
+	pure_source.fire(test_val);
+	BOOST_CHECK_EQUAL(test_val, test_out);
+	}
+
+	{
+	float test_out = 0.0;
+	tests::owning_node root;
+	auto tree_terminal = root.make_child_named<event_terminal<float>>("terminal");
+
+	event_source<float> tree_source{&root.node()};
+	event_sink<float> tree_sink{&root.node(), [&test_out](float in)
+		{test_out = in;}};
+
+	tree_source >> tree_terminal->in();
+	tree_terminal->out() >> tree_sink;
+
+	tree_source.fire(test_val);
+	BOOST_CHECK_EQUAL(test_val, test_out);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(test_state_terminal)
+{
+	const float test_val = 1.234;
+
+	state_terminal<float, pure::pure_node> pure_terminal;
+
+	pure::state_source<float> pure_source{[test_val](){ return test_val;}};
+	pure::state_sink<float> pure_sink;
+
+	pure_source >> pure_terminal.in();
+	pure_terminal.out() >> pure_sink;
+
+	BOOST_CHECK_EQUAL(test_val, pure_sink.get());
+
+	tests::owning_node root;
+	auto tree_terminal = root.make_child_named<state_terminal<float>>("terminal");
+
+	state_source<float> tree_source{&root.node(),[test_val](){ return test_val;}};
+	state_sink<float> tree_sink{&root.node()};
+
+	tree_source >> tree_terminal->in();
+	tree_terminal->out() >> tree_sink;
+
+	BOOST_CHECK_EQUAL(test_val, tree_sink.get());
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
