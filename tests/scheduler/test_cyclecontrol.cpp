626612cf3f6fcb296441fa4418e60c9ce6b23d2e
Jeremi Piotrowski <jeremi.piotrowski@gmail.com>
2016-07-04 15:41:17 +0200

    tests: Fix fast_main_loop test to not fail on slow machines
    Previously the test would let tasks run for 10ms, with the assumption
    that this is enough time for slow tasks to run atleast once. On slow
    machines (such as some jenkins build nodes) this is not true.
    
    Now a std::promise is used to block the test thread until the slow task
    has had the chance to run. I tested that this test doesn't fail anymore
    by running (on an 8 core machien):
    
        ( one terminal )
        stress --cpu 16
    
        ( other terminal)
        for i in `seq 1 100`; do
            ./tests/test_executable &
        done
        wait

--- tests/scheduler/test_cyclecontrol.cpp
+++ tests/scheduler/test_cyclecontrol.cpp
@@ -14,6 +14,7 @@
 #include <chrono>
 #include <iomanip>
 #include <ctime>
+#include <future>
 #include <unistd.h>
 
 using namespace fc;
@@ -71,11 +72,19 @@ BOOST_AUTO_TEST_CASE(test_fast_main_loop)
 	auto count_fast = 0ull;
 	auto count_medium = 0ull;
 	auto count_slow = 0ull;
+	auto promise = std::promise<void>{};
+	auto block = promise.get_future();
 	controller.add_task({[&] { ++count_fast; }}, cycle::fast_tick);
 	controller.add_task({[&] { ++count_medium; }}, cycle::medium_tick);
-	controller.add_task({[&] { ++count_slow; }}, cycle::slow_tick);
+	controller.add_task({[&, b = false]() mutable {
+		++count_slow;
+		if (!b) {
+			promise.set_value();
+			b = true;
+		}
+	}}, cycle::slow_tick);
 	controller.start(true);
-	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	block.get();
 	controller.stop();
 	auto ratio_fast_medium = static_cast<double>(count_fast) / count_medium;
 	auto ratio_medium_slow = static_cast<double>(count_medium) / count_slow;
