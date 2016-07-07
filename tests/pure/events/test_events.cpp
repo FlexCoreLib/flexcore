FlexCoreLib/flexcore

commit 4bd7b5eb617f13138e451b5e1a99dbe0f1e70f52

traits: Use source information in new is_passive_sink trait
It's very hard to check wheter a generic lambda is a passive sink. This
led connections like this:

    event_source<int> x;
    x >> [](auto) {};

to not result in a port_connection but instead in an
active_connection_proxy. This meant that when x.fire() was called
nothing happened, because the connection was never fully established.

This came down to the checks for is_passive_sink not working for generic
lambdas. But if we use the information on the source part, we can check
if the right-hand side behaves like a proper sink. This is what the new
'is_passive_sink_for' trait does.

This trait is now used in active_passive_connect_impl and
active_connection_proxy.

flexcore/core/traits.hpp
@@ -345,6 +345,13 @@ struct is_passive_source_impl<T, std::enable_if_t<void_callable<T>(0)>> : std::t
 {
 };
 
+template <class T, class Arg, typename = void>
+struct is_passive_sink_for : std::false_type {};
+template <class T>
+struct is_passive_sink_for<T, void, std::result_of_t<T()>> : std::true_type {};
+template <class T, class Arg>
+struct is_passive_sink_for<T, Arg, std::result_of_t<T(Arg)>> : std::true_type {};
+
 template<class T, class enable = void>
 struct is_passive_sink_impl: std::false_type
 {

flexcore/pure/detail/active_connection_proxy.hpp
@@ -106,11 +106,8 @@ struct active_connection_proxy
 	template<class new_passive_t, class enable = void>
 	auto connect(new_passive_t&& new_passive,
 	             std::enable_if_t<(is_passive_source<std::decay_t<new_passive_t>>{} or
-	                               is_passive_sink<std::decay_t<new_passive_t>>{})>* = nullptr) &&
+	                               is_passive_sink_for<std::decay_t<new_passive_t>, result_t>{})>* = nullptr) &&
 	{
-		static_assert(is_passive<std::decay_t<new_passive_t>>{},
-				"new_passive_t in proxy needs to be passive connectable");
-
 		auto tmp = connect_policy()(std::forward<passive_t>(stored_passive),
 		                            std::forward<new_passive_t>(new_passive));
 		return std::forward<active_t>(active).connect(std::move(tmp));
@@ -128,7 +125,7 @@ struct active_connection_proxy
 	 */
 	template <class new_connectable_t,
 	          class = std::enable_if_t<not(is_passive_source<std::decay_t<new_connectable_t>>{} or
-	                                       is_passive_sink<std::decay_t<new_connectable_t>>{})>>
+	                                       is_passive_sink_for<std::decay_t<new_connectable_t>, result_t>{})>>
 	auto connect(new_connectable_t&& new_connectable) &&
 	{
 		auto connection = connect_policy()(std::forward<passive_t>(stored_passive),
@@ -183,7 +180,8 @@ struct active_passive_connect_impl
 		std::enable_if_t
 			<	!is_instantiation_of< active_connection_proxy,std::decay_t<active_t>>{}
 				&&	(	(	is_active_source<std::decay_t<active_t>>{}
-						&& !fc::is_passive_sink<std::decay_t<passive_t>>{}
+						&& !fc::is_passive_sink_for<std::decay_t<passive_t>,
+						                            typename std::decay_t<active_t>::result_t>{}
 						)
 					||	(	fc::is_active_sink<std::decay_t<active_t>>{}
 						&& !fc::is_passive_source<std::decay_t<passive_t>>{}
@@ -213,7 +211,8 @@ struct active_passive_connect_impl
 		std::enable_if_t
 			<	!is_instantiation_of< active_connection_proxy, std::decay_t<active_t>>{}
 				&&	(	(	is_active_source<std::decay_t<active_t>>{}
-						&&	fc::is_passive_sink<std::decay_t<passive_t>>{}
+						&& fc::is_passive_sink_for<std::decay_t<passive_t>,
+						                            typename std::decay_t<active_t>::result_t>{}
 					)
 					||	(	fc::is_active_sink<std::decay_t<active_t>>{}
 						&&	fc::is_passive_source<std::decay_t<passive_t>>{}

commit 488b66b37ec7ffbf66b06be0d83c49d79f409c48

tests: Check that connecting lambdas with type changes works
This has been broken since commit 4bd7b5eb617f but the error actually
comes from the wrong implementation of

    active_source_first::sink::result_t;

which returns the result_t of source (!!) instead of sink. We did it
this way because it is impossible to determine the result type of sink
by _only_ looking at it.

Since then we figured out that we can get the result_t of a sink by
checking what it returns when called with result_t of source. The only
thing we missed was this wrong typedef.

Will be fixed in the following commit.

tests/pure/events/test_events.cpp
@@ -436,4 +436,59 @@ BOOST_AUTO_TEST_CASE(lambda_as_sink)
 	BOOST_CHECK(is_passive_sink_for);
 }
 
+namespace
+{
+struct strongly_typed
+{
+	explicit strongly_typed(double) {}
+};
+}
+
+BOOST_AUTO_TEST_CASE( type_changing_lambdas )
+{
+	pure::event_source<double> src;
+	bool called_1 = false;
+	bool called_2 = false;
+	src >> [&](double d) {
+		called_1 = true;
+		return strongly_typed{d};
+	} >> [&](strongly_typed) {
+		called_2 = true;
+	};
+	src.fire(1.0);
+	BOOST_CHECK(called_1);
+	BOOST_CHECK(called_2);
+}
+
+BOOST_AUTO_TEST_CASE( type_changing_lambda_with_void )
+{
+	pure::event_source<void> src;
+	bool called_1 = false;
+	bool called_2 = false;
+	src >> [&]() {
+		called_1 = true;
+		return strongly_typed{1.0};
+	} >> [&](strongly_typed) {
+		called_2 = true;
+	};
+	src.fire();
+	BOOST_CHECK(called_1);
+	BOOST_CHECK(called_2);
+}
+
+BOOST_AUTO_TEST_CASE( type_changing_lambda_without_void )
+{
+	pure::event_source<double> src;
+	bool called_1 = false;
+	bool called_2 = false;
+	src >> [&](double d) {
+		called_1 = true;
+		return strongly_typed{d};
+	} >> [&](strongly_typed) {
+		called_2 = true;
+	};
+	src.fire(1.0);
+	BOOST_CHECK(called_1);
+	BOOST_CHECK(called_2);
+}
 BOOST_AUTO_TEST_SUITE_END()