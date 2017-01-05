885d1436b5df2ba1c3a11f7ac319ef7fde9dedc8
Alexander Theißen <alex@theissen.io>
2017-01-05 10:16:40 +0100

    Converted initialization in demangle file to indurad style to it does not look like a block

--- flexcore/utils/demangle.cpp
+++ flexcore/utils/demangle.cpp
@@ -8,10 +8,9 @@
 std::string demangle(const char* name)
 {
 	int status = -1;
-	std::unique_ptr<char, void(*)(void*)> res
-	{
-		abi::__cxa_demangle(name, nullptr, nullptr, &status),
-		std::free
+	std::unique_ptr<char, void(*)(void*)> res {
+			abi::__cxa_demangle(name, nullptr, nullptr, &status),
+			std::free
 	};
 	return status == 0 ? res.get() : name;
 }
