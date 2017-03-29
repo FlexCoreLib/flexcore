70b9927772493adc72f2e2f68054d6d7c41a62c8
Caspar Kielwein <Caspar@Kielwein.de>
2017-03-29 22:30:43 +0200

    removed dead code in connection_util.hpp

--- flexcore/core/connection_util.hpp
+++ flexcore/core/connection_util.hpp
@@ -1,8 +1,9 @@
 #ifndef SRC_PORTS_CONNECTION_UTIL_HPP_
 #define SRC_PORTS_CONNECTION_UTIL_HPP_
 
-#include <utility>
 #include <flexcore/core/traits.hpp>
+
+#include <utility>
 
 namespace fc
 {
@@ -12,7 +13,7 @@ namespace fc
  * Stopping criterion for recursion
  */
 template <class T>
-auto get_source(T& s)
+constexpr auto get_source(T& s)
 	-> std::enable_if_t<!has_source<T>(0), decltype(s)>
 {
 	return s;
@@ -21,7 +22,7 @@ auto get_source(T& s)
  * \brief recursively extracts the source of a connection
  */
 template <class T>
-auto get_source(T& c)
+constexpr auto get_source(T& c)
 	-> std::enable_if_t<has_source<T>(0),
 			decltype(get_source(c.source))>
 {
@@ -33,7 +34,7 @@ auto get_source(T& c)
  * Stopping criterion for recursion
  */
 template <class T>
-auto get_sink(T& s)
+constexpr auto get_sink(T& s)
 	-> std::enable_if_t<!has_sink<T>(0), decltype(s)>
 {
 	return s;
@@ -43,54 +44,12 @@ auto get_sink(T& s)
  * \brief recursively extracts the sink of an object with member "sink"
  */
 template <class T>
-auto get_sink(T& c)
+constexpr auto get_sink(T& c)
 	-> std::enable_if_t<has_sink<T>(0),
 			decltype(get_sink(c.sink))>
 {
 	return get_sink(c.sink);
 }
-
-/**
- * \brief gets the source type of the connectalbe
- * \pre connectable is either a connection or a source
- */
-template <class T, class Enable = void>
-struct get_source_t
-{
-	typedef T type;
-};
-
-/**
- * \brief specialization for the case of a connection
- */
-template <class T>
-struct get_source_t<T,
-		std::enable_if_t< has_source<T>(0) >
-	>
-{
-	typedef typename get_source_t<decltype(std::declval<T>().source)>::type type;
-};
-
-/**
- * \brief gets the sink type of the connectalbe
- * \pre connectable is either a connection or a sink
- */
-template <class T, class Enable = void>
-struct get_sink_t
-{
-	typedef T type;
-};
-
-/**
- * \brief specialization for the case of a connection
- */
-template <class T>
-struct get_sink_t<T,
-		std::enable_if_t< has_sink<T>(0) >
-	>
-{
-	typedef typename get_sink_t<decltype(std::declval<T>().sink)>::type type;
-};
 
 }// namespace fc
 #endif /* SRC_PORTS_CONNECTION_UTIL_HPP_ */
